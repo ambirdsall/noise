a352766572f3978cad98487bc4b3d415539d4ab8
ambirdsall <ambirdsall@gmail.com>
2016-07-27 14:30:10 -0700

    Prepare for duration building

---
+++ j/durations.js
@@ -0,0 +1,6 @@
+var _ =
+  require('lodash')
+
+// In combinatorial terms, I suppose I want to build a bluebird: a higher-order
+// function that takes a set of functions which play notes/chords and calls
+// them in sequence
